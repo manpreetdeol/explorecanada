65d29492f7df39c6a4bcc5b6dd259cf0a474d89c
manpreetdeol <manpreetsingh.16.11.87@gmail.com>
2020-04-08 17:36:56 -0400

    ADD: Initial commit with working spring project

---
+++ exploreca/src/test/java/com/example/ec/exploreca/ExplorecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec.exploreca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
