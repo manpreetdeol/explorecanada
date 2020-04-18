2553318eb12a6fbfa19960db84b28cfc5f788f82
manpreetdeol <manpreetsingh.16.11.87@gmail.com>
2020-04-18 02:52:15 -0400

    Added JWT to application

--- exploreca/src/main/java/com/example/ec/exploreca/security/WebSecurityConfiguration.java
+++ exploreca/src/main/java/com/example/ec/exploreca/security/WebSecurityConfiguration.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.ec.exploreca.repo.RoleRepository;
 
@@ -19,6 +21,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     RoleRepository roleRepository;
+    @Autowired
+    private ExploreCaliUserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,7 +41,20 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // No session will be created or used by spring security
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
+        http.addFilterBefore(new JwtTokenFilter(userDetailsService), UsernamePasswordAuthenticationFilter.class);
 
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Allow swagger to be accessed without authentication
+        web.ignoring().antMatchers("/v2/api-docs")//
+                .antMatchers("/swagger-resources/**")//
+                .antMatchers("/swagger-ui.html")//
+                .antMatchers("/configuration/**")//
+                .antMatchers("/webjars/**")//
+                .antMatchers("/public");
     }
 
     @Bean
--- exploreca/src/main/java/com/example/ec/exploreca/service/UserService.java
+++ exploreca/src/main/java/com/example/ec/exploreca/service/UserService.java
@@ -4,10 +4,12 @@ package com.example.ec.exploreca.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +17,12 @@ import com.example.ec.exploreca.domain.Role;
 import com.example.ec.exploreca.domain.User;
 import com.example.ec.exploreca.repo.RoleRepository;
 import com.example.ec.exploreca.repo.UserRepository;
+import com.example.ec.exploreca.security.JwtProvider;
 
 @Service
 public class UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     private UserRepository userRepository;
 
@@ -27,17 +32,31 @@ public class UserService {
 
     private PasswordEncoder passwordEncoder;
 
+    private JwtProvider jwtProvider;
+
     @Autowired
     public UserService(UserRepository userRepository, AuthenticationManager authenticationManager,
-                       RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+                       RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtProvider jwtProvider) {
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtProvider = jwtProvider;
     }
 
-    public Authentication signin(String username, String password) {
-        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    public Optional<String> signin(String username, String password) {
+        LOGGER.info("New user attempting to sign in");
+        Optional<String> token = Optional.empty();
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            try {
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+                token = Optional.of(jwtProvider.createToken(username, user.get().getRoles()));
+            } catch (AuthenticationException e){
+                LOGGER.info("Log in failed for user {}", username);
+            }
+        }
+        return token;
     }
     
     public Optional<User> signup(String username, String password, String firstName, String lastName) {
